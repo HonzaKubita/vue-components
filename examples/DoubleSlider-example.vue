84b31ea25840d08b14bdb26dcb7edefb15882e2b
Honza Kubita <honza.kubita@gmail.com>
2023-02-06 16:20:48 +0100

    DoubleSlider v1

---
+++ examples/DoubleSlider-example.vue
@@ -0,0 +1,34 @@
+<template>
+
+<p>Min: {{ range.min }} | Max: {{ range.max }}</p>
+
+<DoubleSlider :min="0" :max="100" :step="1" v-model="range"/>
+
+</template>
+
+<!-- Composition API -->
+<script setup>
+
+const range = reactive({ // Define a reactive for accessing the values
+  min: 0,   // Position of the left slider (minimum)
+  max: 100  // Position of the right slider (maximum)
+})
+
+</script>
+
+<!-- Options API -->
+
+<!--
+<script>
+
+export default {
+  data: () => ({
+    range: {
+      min: 0,  // Position of the left slider (minimum)
+      max: 100 // Position of the right slider (maximum)
+    }
+  })
+}
+
+</script>
+-->
---
+++ src/DoubleSlider.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="double-sliders">
+    <input 
+      type="range" 
+      class="double-slider" 
+      :min="props.min" 
+      :max="props.max" 
+      :step="props.step"
+      v-model.number="min"
+      @input="validateDoubleSlider('min')"
+    >
+    <input 
+      type="range" 
+      class="double-slider" 
+      :min="props.min" 
+      :max="props.max"
+      :step="props.step" 
+      v-model.number="max" 
+      @input="validateDoubleSlider('max')"
+    >
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps(["modelValue", "max", "min", "step"]);
+const emit = defineEmits(["update:modelValue"]);
+
+const min = ref(props.modelValue.min);
+const max = ref(props.modelValue.max);
+
+function validateDoubleSlider(side) {
+  if (side == "min" && min.value > max.value) {
+    min.value = max.value;
+  }
+  else if (side == "max" && max.value < min.value) {
+    max.value = min.value;
+  }
+
+  let value = {min: min.value, max: max.value}
+
+  emit('update:modelValue', value);
+}
+
+</script>
+
+
+<style>
+
+.double-sliders {
+  width: 90%;
+
+  position: relative;
+}
+
+.double-slider {
+  width: 90%;
+  position: relative;
+}
+
+.double-slider {
+  -webkit-appearance: none;
+  background: transparent; 
+  position: absolute;
+  left: 0;
+}
+
+.double-slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  background: black;
+  cursor: pointer;
+  margin-top: -5px;
+  position: relative;
+  z-index: 1;
+}
+
+.double-slider::-webkit-slider-runnable-track {
+  width: 100%;
+  height: 5px;
+  background: #e8e8e8;
+  border-radius: 3px;
+  border: none;
+}
+
+</style>
